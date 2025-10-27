f5a031ade924e3f7a0e27f3aab7efac53f498a67
HiemSoheng <hiemsoheng003@gmail.com>
2025-10-27 22:14:37 +0700

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,7 +41,7 @@ const NavBar = () => {
           <Link to="/Cart" className="relative">
             <FaCartShopping size={25} className="flex justify-items-end" />
             {cart.length > 0 && (
-              <span className="absolute -top-2 -right-2 text-red-500 rounded-full w-5 h-5 flex items-center justify-center text-xs">
+              <span className="absolute -top-2 -right-2 bg-red-700 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
                 {cart.reduce((total, item) => total + item.quantity, 0)}
               </span>
             )}
